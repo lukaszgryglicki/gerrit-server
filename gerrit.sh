7514497fe04340fbfe29a1f10fa9608100220846
Lukasz Gryglicki <lgryglicki@cncf.io>
2025-02-18 09:06:21 +0000

    Logs and canonical URL
    Signed-off-by: Lukasz Gryglicki <lgryglicki@cncf.io>

--- gerrit.sh
+++ gerrit.sh
@@ -9,6 +9,7 @@ do
     -v $(pwd)/data/etc:/var/gerrit/etc \
     -v $(pwd)/data/git:/var/gerrit/git \
     -v $(pwd)/data/index:/var/gerrit/index \
+    -e CANONICAL_WEB_URL='http://147.75.85.27:8080/' \
     gerritcodereview/gerrit \
     1>"gerrit-${dt}.log" \
     2>"gerrit-${dt}.err"
---
+++ logs.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+tail -f gerrit-1*.???
