db5de621a4e0cfb046c99acc7b230bbeaf51f8ef
Lukasz Gryglicki <lgryglicki@cncf.io>
2025-03-04 07:28:39 +0000

    Typo
    Signed-off-by: Lukasz Gryglicki <lgryglicki@cncf.io>

--- gerrit-run.sh
+++ gerrit-run.sh
@@ -7,6 +7,6 @@ docker run -p 8080:8080 -p 29418:29418 \
   -v $(pwd)/data/index:/var/gerrit/index \
   -v $(pwd)/data/lib:/var/gerrit/lib \
   -e CANONICAL_WEB_URL='http://147.75.85.27:8080/' \
-#  -e HTTPD_LISTEN_URL='proxy-https://*:8080/' \
   -e HTTPD_LISTEN_URL='proxy-http://*:8080/' \
   gerritcodereview/gerrit
+#  -e HTTPD_LISTEN_URL='proxy-https://*:8080/' \
